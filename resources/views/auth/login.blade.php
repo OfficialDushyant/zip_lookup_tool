5f76c62462f59ff2c7d7599590b38e15529bfce0
Dushyant Patel <devops@bydushyant.com>
2022-10-23 14:09:33 -0400

    refactor 🛠: Changing start point of application
    - Removing Default welcome page at '/'
    - redirect unauthorized user to login page and logged in user to dashboard
    - add registration link to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,19 +35,24 @@
 
             <!-- Remember Me -->
             <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
+                    <label for="remember_me" class="flex items-center">
+                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+                        <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                        <span class="ml-3">
+                            @if (Route::has('password.request'))
+                                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                                    {{ __('Forgot your password?') }}
+                                </a>
+                            @endif
+                        </span>
+                    </label>
             </div>
+            
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
+            <div class="flex items-center justify-start mt-4">
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Not a member yet, You can Register here.</a>
                 @endif
-
                 <x-primary-button class="ml-3">
                     {{ __('Log in') }}
                 </x-primary-button>
